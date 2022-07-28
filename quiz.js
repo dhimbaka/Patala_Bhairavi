a65394fdd54fe816901c89d8a5c95484d3346443
Kiichi Takeuchi <info@xxki.com>
2022-07-28 11:09:00 -0400

    fixing null reference bug and formatting + refactoring

--- quiz.js
+++ quiz.js
@@ -1,100 +1,102 @@
-    function resetQuiz() {
-    document.getElementById("timer").innerHTML = "";    
-    document.getElementById("qNo").textContent=parseInt(localStorage.qNo);
+// Note: This is legacy way to include in script
+// GLOBAL VARIABLES
+var timerId = setInterval(countdown, 1000);
+var timeLeft = 10; //time per question = 10 seconds
+
+function resetQuiz() {
+    document.getElementById("timer").innerHTML = "";
+    document.getElementById("qNo").textContent = parseInt(localStorage.qNo);
 
     const collection = document.getElementsByClassName("options");
     for (let i = 0; i < collection.length; i++) {
-    collection[i].style.backgroundColor = "aliceblue"
-    collection[i].setAttribute("style","cursor: pointer")
+        collection[i].style.backgroundColor = "aliceblue"
+        collection[i].setAttribute("style", "cursor: pointer")
     }
 
-    document.getElementById("option1").setAttribute("onclick","option1()")
-    document.getElementById("option2").setAttribute("onclick","option2()")
-    document.getElementById("option3").setAttribute("onclick","option3()")
-    document.getElementById("option4").setAttribute("onclick","option4()")
-    document.getElementById("next").style.display="none";
+    document.getElementById("option1").setAttribute("onclick", "option1()")
+    document.getElementById("option2").setAttribute("onclick", "option2()")
+    document.getElementById("option3").setAttribute("onclick", "option3()")
+    document.getElementById("option4").setAttribute("onclick", "option4()")
+    document.getElementById("next").style.display = "none";
 
     clearInterval(timerId);
     resetCountdown();
-    scoreDisplay();     
-    } 
+    scoreDisplay();
+}
 
-    var timerId = setInterval(countdown, 1000);
-    var timeLeft = 10; //time per question = 10 seconds
-
-    function countdown() {
-      if (timeLeft == -1) {
+function countdown() {
+    if (timeLeft == -1) {
         clearInterval(timerId);
         chooseAnswer();
-      } else {
+    } else {
         document.getElementById("timer").innerHTML = timeLeft;
-        console.log("time left = "+timeLeft);
+        console.log("time left = " + timeLeft);
         timeLeft--;
-      }
     }
+}
 
-    function option1() {
-        if(localStorage.answer!=1){
-            document.getElementById("option1").style.backgroundColor="red";
-        } else{
-            localStorage.quizScore++;
-            scoreDisplay();
+function option1() {
+    if (localStorage.answer != 1) {
+        document.getElementById("option1").style.backgroundColor = "red";
+    } else {
+        localStorage.quizScore++;
+        scoreDisplay();
+    }
+    chooseAnswer();
+}
+
+function option2() {
+    if (localStorage.answer != 2) {
+        document.getElementById("option2").style.backgroundColor = "red";
+    } else {
+        localStorage.quizScore++;
+        scoreDisplay();
+    }
+    chooseAnswer();
+}
+
+function option3() {
+    if (localStorage.answer != 3) {
+        document.getElementById("option3").style.backgroundColor = "red";
+    } else {
+        localStorage.quizScore++;
+        scoreDisplay();
+    }
+    chooseAnswer();
+}
+
+function option4() {
+    if (localStorage.answer != 4) {
+        document.getElementById("option4").style.backgroundColor = "red";
+    } else {
+        localStorage.quizScore++;
+        scoreDisplay();
+    }
+    chooseAnswer();
+}
+
+function chooseAnswer() {
+    const collection = document.getElementsByClassName("options");
+    for (let i = 0; i < collection.length; i++) {
+        collection[i].removeAttribute('onclick')
+        collection[i].style.cursor = "default"
+        if (localStorage.answer == i + 1) {
+            collection[i].style.backgroundColor = "#8BC34A";
         }
-        chooseAnswer();
     }
+    document.getElementById("next").style.display = "block";
+    clearInterval(timerId);
+    console.log("answer = " + localStorage.answer);
+}
 
-    function option2() {
-        if(localStorage.answer!=2){
-            document.getElementById("option2").style.backgroundColor="red";
-        } else{
-            localStorage.quizScore++;
-            scoreDisplay();
-        }
-        chooseAnswer();
+function scoreDisplay() {
+    if (localStorage.quizScore == null || isNaN(localStorage.quizScore)) {
+        localStorage.quizScore = 0;
     }
+    document.getElementById("score").textContent = parseInt(localStorage.quizScore);
+}
 
-    function option3() {
-        if(localStorage.answer!=3){
-            document.getElementById("option3").style.backgroundColor="red";
-        } else{
-            localStorage.quizScore++;
-            scoreDisplay();
-        }
-        chooseAnswer();
-    }
-
-    function option4() {
-        if(localStorage.answer!=4){
-            document.getElementById("option4").style.backgroundColor="red";
-        } else{
-            localStorage.quizScore++;
-            scoreDisplay();
-        }
-        chooseAnswer();
-    }
-
-    function chooseAnswer() {
-        const collection = document.getElementsByClassName("options");
-        for (let i = 0; i < collection.length; i++) {
-            collection[i].removeAttribute('onclick')
-            collection[i].style.cursor="default"
-            if(localStorage.answer==i+1){
-            collection[i].style.backgroundColor="#8BC34A";
-            }   
-        }       
-        document.getElementById("next").style.display="block";
-        clearInterval(timerId);
-        console.log("answer = "+localStorage.answer);
-    }
-
-    function scoreDisplay(){
-        if (localStorage.quizScore==null||isNaN(localStorage.quizScore)){
-            localStorage.quizScore=0;
-        }
-        document.getElementById("score").textContent=parseInt(localStorage.quizScore);
-    }
-
-    function resetCountdown() {
-    timeLeft=10;
+function resetCountdown() {
+    timeLeft = 10;
     timerId = setInterval(countdown, 1000);
-    }
+}
--- services.js
+++ services.js
@@ -1,111 +1,115 @@
 // Common Services - this could be shared in different pages
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
-    const SUPABASE_URL = 'https://letehqikkyplgnppugbg.supabase.co'
-    const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxldGVocWlra3lwbGducHB1Z2JnIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTU0MTU1MTMsImV4cCI6MTk3MDk5MTUxM30.uZ2whAkxVRv0Raub5dLLFJO1S-9ude6ljezpzKzkC8w'    
-    const _supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+const SUPABASE_URL = 'https://letehqikkyplgnppugbg.supabase.co'
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxldGVocWlra3lwbGducHB1Z2JnIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTU0MTU1MTMsImV4cCI6MTk3MDk5MTUxM30.uZ2whAkxVRv0Raub5dLLFJO1S-9ude6ljezpzKzkC8w'
+const _supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
-    async function setNo(){
-            const { data, error } = await _supabase
-            .from('public_quiz')
-            .select('set_no')
-            .limit(1)
-            .order('set_no', { ascending: false })
-           
-            if(!error) {
-            data.forEach(function(item){
-            localStorage.onlineSet=item.set_no})
-            } 
-            console.log("online set no = "+localStorage.onlineSet);
-            console.log("last set no = "+localStorage.lastSet);
-
-            function startButton(){
-
-                if(localStorage.lang==null){
-                    document.getElementById("startQ").style.display="none";
-                    document.getElementById("message").style.display="none";  
-                    return    
-                }   
-            
-                //typecast into int since localstorage.lastset is not an integer
-                if(parseInt(localStorage.lastSet) == parseInt(localStorage.onlineSet)) {
-                    document.getElementById("startQ").style.display="none";
-                    document.getElementById("message").style.display="block";  
-                    return    
-                    } 
-                
-                document.getElementById("startQ").style.display="block";
-                document.getElementById("message").style.display="none";    
-            }      
-            
-            function tel() {
-                localStorage.lang = 'tel';
-                document.getElementById("logo").src="./resources/images/tel_logo1.png";
-                document.getElementById('tel').style.color="#FFC107";
-                document.getElementById('eng').style.color="white";
-                document.getElementById("message").textContent="రేపు రా ఢింబక";
-                document.getElementById("startQ").textContent="సాహసం చేయరా ఢింబక!"
-                startButton();
-                }
-            
-            function eng() {
-                localStorage.lang = 'eng';
-                document.getElementById("logo").src="./resources/images/eng_logo1.png";
-                document.getElementById('tel').style.color="white";
-                document.getElementById('eng').style.color="#FFC107";
-                document.getElementById("message").textContent="No more quizzes today"
-                document.getElementById("startQ").textContent="Go forth!";
-                startButton();    
-                }
-                
-            startButton();
-            if(localStorage.lang=='tel'){tel()};
-            if(localStorage.lang=='eng'){eng()};
-            
-            document.getElementById('tel').addEventListener('click',tel);
-            document.getElementById('eng').addEventListener('click',eng);
-
-            }
-
-    async function currentquiz(){
-
-        if (localStorage.qNo>9){ //reset all stats at the end of each set
-            if (localStorage.totalScore==null||isNaN(localStorage.totalScore)){
-                localStorage.totalScore=0;
-                localStorage.percent=0;
-            } 
-            localStorage.totalScore = parseInt(localStorage.totalScore) + parseInt(localStorage.quizScore);
-            localStorage.lastSet = parseInt(localStorage.lastSet)+1;
-            localStorage.percent = (parseInt(localStorage.totalScore)*100)/(parseInt(localStorage.lastSet)*10);
-            localStorage.removeItem("qNo");
-            localStorage.removeItem("quizScore");
-            localStorage.removeItem("localStorage.answer");
-            location.href = 'scores.html';
-            return;
-        }
-    
-        if (localStorage.lastSet==null||isNaN(localStorage.lastSet)){
-            localStorage.lastSet=0;
-            }
-
-    if (localStorage.qNo==null||isNaN(localStorage.qNo)){
-    localStorage.qNo=1;
-    } else {localStorage.qNo++}
-
-        const {data, error} = await _supabase
+async function pageController() {
+    const { data, error } = await _supabase
         .from('public_quiz')
-        .select('*')
-        .eq('set_no', parseInt(localStorage.lastSet)+1)
-        .eq('q_no', parseInt(localStorage.qNo));
-        
-        if (error){
-        // TODO: handle it
-        location.href = 'index.html';
-        localStorage.qNo--; 
-        console.log('error',error);
+        .select('set_no')
+        .limit(1)
+        .order('set_no', { ascending: false })
+
+    if (!error) {
+        data.forEach(function (item) {
+            localStorage.onlineSet = item.set_no
+        });
+    }
+    console.log("online set no = " + localStorage.onlineSet);
+    console.log("last set no = " + localStorage.lastSet);
+
+    function startButton() {
+
+        if (localStorage.lang == null) {
+            document.getElementById("startQ").style.display = "none";
+            document.getElementById("message").style.display = "none";
+            return
         }
-        console.log("running set #"+(parseInt(localStorage.lastSet)+1));
-        return data[0];
+
+        //typecast into int since localstorage.lastset is not an integer
+        if (parseInt(localStorage.lastSet) == parseInt(localStorage.onlineSet)) {
+            document.getElementById("startQ").style.display = "none";
+            document.getElementById("message").style.display = "block";
+            return
+        }
+
+        document.getElementById("startQ").style.display = "block";
+        document.getElementById("message").style.display = "none";
     }
 
-export {setNo,currentquiz}
+    function tel() {
+        localStorage.lang = 'tel';
+        document.getElementById("logo").src = "./resources/images/tel_logo1.png";
+        document.getElementById('tel').style.color = "#FFC107";
+        document.getElementById('eng').style.color = "white";
+        document.getElementById("message").textContent = "రేపు రా ఢింబక";
+        document.getElementById("startQ").textContent = "సాహసం చేయరా ఢింబక!"
+        startButton();
+    }
+
+    function eng() {
+        localStorage.lang = 'eng';
+        document.getElementById("logo").src = "./resources/images/eng_logo1.png";
+        document.getElementById('tel').style.color = "white";
+        document.getElementById('eng').style.color = "#FFC107";
+        document.getElementById("message").textContent = "No more quizzes today"
+        document.getElementById("startQ").textContent = "Go forth!";
+        startButton();
+    }
+
+    // Init Process
+    startButton();
+    if (localStorage.lang == 'tel') { tel() };
+    if (localStorage.lang == 'eng') { eng() };
+
+    document.getElementById('tel').addEventListener('click', tel);
+    document.getElementById('eng').addEventListener('click', eng);
+}
+
+// Pre Condition: Pointing to the current quiz on the screen
+// Post Condition: Increment to the next quiz
+// Return: the next Quiz
+async function loadNextQuiz() {
+
+    if (localStorage.qNo > 9) { //reset all stats at the end of each set
+        if (localStorage.totalScore == null || isNaN(localStorage.totalScore)) {
+            localStorage.totalScore = 0;
+            localStorage.percent = 0;
+        }
+        localStorage.totalScore = parseInt(localStorage.totalScore) + parseInt(localStorage.quizScore);
+        localStorage.lastSet = parseInt(localStorage.lastSet) + 1;
+        localStorage.percent = (parseInt(localStorage.totalScore) * 100) / (parseInt(localStorage.lastSet) * 10);
+        localStorage.removeItem("qNo");
+        localStorage.removeItem("quizScore");
+        localStorage.removeItem("localStorage.answer");
+        location.href = 'scores.html';
+        return;
+    }
+
+    if (localStorage.lastSet == null || isNaN(localStorage.lastSet)) {
+        localStorage.lastSet = 0;
+    }
+
+    if (localStorage.qNo == null || isNaN(localStorage.qNo)) {
+        localStorage.qNo = 1;
+    } else { localStorage.qNo++ }
+
+    const { data, error } = await _supabase
+        .from('public_quiz')
+        .select('*')
+        .eq('set_no', parseInt(localStorage.lastSet) + 1)
+        .eq('q_no', parseInt(localStorage.qNo));
+
+    if (error) {
+        // TODO: handle it
+        location.href = 'index.html';
+        localStorage.qNo--;
+        console.log('error', error);
+    }
+    console.log("running set #" + (parseInt(localStorage.lastSet) + 1));
+    return data[0];
+}
+
+export { pageController, loadNextQuiz }
